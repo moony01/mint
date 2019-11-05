59f443956010c4db5fcccf88568667f6a9d9e24e
soyeon <52073383+hhhongso@users.noreply.github.com>
2019-11-05 09:25:20 +0900

    suggestBoard, mybatis-config 수정

--- mintProject/src/main/java/mint/suggestBoard/dao/SuggestBoardDAO.java
+++ mintProject/src/main/java/mint/suggestBoard/dao/SuggestBoardDAO.java
@@ -1,9 +1,16 @@
 package mint.suggestBoard.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import mint.suggestBoard.bean.SuggestBoardDTO;
 
 public interface SuggestBoardDAO {
 
 	public void writeSuggestBoard(SuggestBoardDTO suggestBoardDTO);
+
+	public List<SuggestBoardDTO> getSuggestBoardList(Map<String, Integer> map);
+
+	public int getSuggestBoardTotArticle();
 
 }
--- mintProject/src/main/java/mint/suggestBoard/service/SuggestBoardServiceImpl.java
+++ mintProject/src/main/java/mint/suggestBoard/service/SuggestBoardServiceImpl.java
@@ -1,5 +1,8 @@
 package mint.suggestBoard.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,17 @@ public class SuggestBoardServiceImpl implements SuggestBoardService {
 	@Override
 	public void writeSuggestBoard(SuggestBoardDTO suggestBoardDTO) {
 		suggestBoardDAO.writeSuggestBoard(suggestBoardDTO);
+	}
+
+	@Override
+	public List<SuggestBoardDTO> getSuggestBoardList(Map<String, Integer> map) {
+		return suggestBoardDAO.getSuggestBoardList(map);
+		
+	}
+
+	@Override
+	public int getSuggestBoardTotArticle() {
+		return suggestBoardDAO.getSuggestBoardTotArticle();
 	}
 
 }

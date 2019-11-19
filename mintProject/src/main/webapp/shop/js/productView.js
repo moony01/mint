0e89c2a9c5397ff6f861298f3834f27cfcd26b27
csh9066 <50465901+csh9066@users.noreply.github.com>
2019-11-19 11:13:35 +0900

    프로덕트 뷰 js 마지막 수정 진짜

--- mintProject/src/main/webapp/shop/js/productView.js
+++ mintProject/src/main/webapp/shop/js/productView.js
@@ -138,10 +138,10 @@ reivewInit();
 
 (function(){
     const saveBtn = document.querySelector(".btn-save"),
-            modal = document.querySelector(".modal"),
-            overlay = modal.querySelector(".modal__overlay"),
-            closeBtn = modal.querySelector(".modal__close-btn"),
-            yesBtn = modal.querySelector(".modal__yes-btn");
+          modal = document.querySelector(".notice-modal"),
+          overlay = modal.querySelector(".notice-modal__overlay"),
+          closeBtn = modal.querySelector(".notice-modal__close-btn"),
+          yesBtn = modal.querySelector(".notice-modal__yes-btn");
 
     function closeModal(){
         modal.classList.add("hidden")

a9ce548259a30b965d0f2e291f608c0fb57fe7c6
HanMoonHee <mun01180@gmail.com>
2019-12-03 15:43:15 +0900

    반응형을위한 main 1차수정(문제될만한 코드 있으면 말씀해주세요)

--- mintProject/src/main/webapp/shop/js/main.js
+++ mintProject/src/main/webapp/shop/js/main.js
@@ -10,18 +10,6 @@ var swiper = new Swiper('.main__banner-slide', {
     },
 });
 
-var swiper = new Swiper('.main__event-slide1', {
-    slidesPerView: 4,
-    slidesPerGroup: 2,
-    speed: 200,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-        nextEl: '#event1-next',
-        prevEl: '#event1-pre',
-    },
-});
-
 var swiper = new Swiper('.main__event-slide2', {
     slidesPerView: 4,
     slidesPerGroup: 2,
@@ -44,3 +32,32 @@ var swiper = new Swiper('.main__event-slide3', {
         prevEl: '#event3-pre',
     },
 });
+
+var windowWidth = $(window).width();
+if(windowWidth < 480 && windowWidth >= 320) {
+	var swiper = new Swiper('.main__event-slide1', {
+	    slidesPerView: 2,
+	    slidesPerGroup: 1,
+	    speed: 200,
+	    loop: true,
+	    navigation: {
+	        nextEl: '#event1-next',
+	        prevEl: '#event1-pre',
+	    },
+	});
+}else {
+	var swiper = new Swiper('.main__event-slide1', {
+	    slidesPerView: 4,
+	    slidesPerGroup: 2,
+	    speed: 200,
+	    spaceBetween: 30,
+	    loop: true,
+	    navigation: {
+	        nextEl: '#event1-next',
+	        prevEl: '#event1-pre',
+	    },
+	});
+}
+
+	
+	

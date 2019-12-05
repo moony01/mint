3cf173b34c6ae7c2246c116d0217dec07378bc08
csh9066 <50465901+csh9066@users.noreply.github.com>
2019-12-05 17:35:44 +0900

    header 버벅거림 이슈 해결

--- mintProject/src/main/webapp/shop/js/index.js
+++ mintProject/src/main/webapp/shop/js/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
     const scrollTop = document.documentElement.scrollTop;
     const gnb = document.querySelector('.gnb');
-    if (scrollTop > 140) {
+    if (scrollTop > 98) {
         gnb.classList.add('fixed');
     } else {
         gnb.classList.remove('fixed');
@@ -36,11 +36,13 @@ function checkMemId(memId, memName) {
 									            	<form id="logoutForm" method="post" action="/mintProject/shop/member/logout">
 														<a href="javascript:void(0)" onclick="document.getElementById('logoutForm').submit();">로그아웃</a></li>
 									            	</form>
-									       	</ul>`);
+									       	</ul>`,
+                );
         } else {
-            $('ul.my-page >:first-child')
-                .attr('class', 'my-page__item my-page__mypage')
-                .append(`<a href="#"> ${memName} 님 </a>
+            $('ul.my-page >:first-child').attr(
+                'class',
+                'my-page__item my-page__mypage',
+            ).append(`<a href="#"> ${memName} 님 </a>
                 		<ul class="my-page__menu">
 							<li><a href="/mintProject/shop/mypage/myOrderList">주문내역</a></li>
 							<li><a href="#">위시 리스트</a></li>
